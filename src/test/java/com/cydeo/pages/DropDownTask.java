e3cfd46920d297631b7ddad69afb376b2c4cdcfa
Alex Akulov <alexyiyi92@gmail.com>
2022-03-24 14:27:29 -0400

    s

--- src/test/java/com/cydeo/pages/DropDownTask.java
+++ src/test/java/com/cydeo/pages/DropDownTask.java
@@ -1,2 +1,16 @@
-package com.cydeo.pages;public class DropDownTask {
+package com.cydeo.pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import static com.cydeo.utilities.DriverSingleton.getDriver;
+
+public class DropDownTask {
+    
+    public DropDownTask() {
+        PageFactory.initElements(getDriver(),this);
+    }
+    @FindBy (id = "month") public WebElement monthDropdown;
+
 }
--- src/test/java/com/cydeo/step_definitions/StepDefs.java
+++ src/test/java/com/cydeo/step_definitions/StepDefs.java
@@ -1,27 +1,46 @@
 package com.cydeo.step_definitions;
 
 
+import com.cydeo.pages.DropDownTask;
 import com.cydeo.utilities.ConfigurationReader;
 import com.cydeo.utilities.DriverSingleton;
+import com.github.dockerjava.api.model.Link;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+
+import java.util.ArrayList;
+import java.util.LinkedHashSet;
+import java.util.List;
 import java.util.Map;
 
 
 public class StepDefs {
+    DropDownTask dropDownTask = new DropDownTask();
     @Given("User is on the login page of CRM")
     public void login(){
         String url = ConfigurationReader.getProperty("url");
         DriverSingleton.getDriver().get(url);
     }
-
     @When("User enters below credentials")
     public void userEntersBelowCredentials(Map<String,String> list) {
         for(Map.Entry<String,String> each: list.entrySet()){
             System.out.println(each.getValue());
         }
+        Select s = new Select(dropDownTask.monthDropdown);
+        List<WebElement> listOfWebElementsOfMonths = s.getOptions();
+        LinkedHashSet<String> listOfStringsOfMonths = new LinkedHashSet<>();
+        for (WebElement each:listOfWebElementsOfMonths){
+            listOfStringsOfMonths.add(each.getText());
+        }
+        System.out.println(listOfStringsOfMonths);
     }
+
+
+
 
     @Then("User should see homepage")
     public void userShouldSeeHomepage() {
